e9c24aae0b33707bd95d8c28faf1c77fb4adda04
Marcus Perdue <marcusperduee@gmail.com>
2025-08-03 20:24:40 -0700

    Removed duplicate entry point files

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  toggleFullscreen: () => ipcRenderer.send('toggle-fullscreen'),
-  getGamepads: () => navigator.getGamepads?.(),
-});
